f405ce7a36bb12b07f90f0bb62ac922623607d94
yusshu <yushu@unnamed.team>
2021-07-23 02:43:25 -0500

    build: add sonatype repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ allprojects {
         mavenLocal()
         maven("https://repo.unnamed.team/repository/unnamed-public/")
         maven("https://repo.codemc.io/repository/nms/")
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+        maven("https://oss.sonatype.org/content/repositories/central")
         mavenCentral()
     }
 }
