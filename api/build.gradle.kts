6735530cda867573e0c7c1b69cb8001a71a012ad
yusshu <yushu@unnamed.team>
2021-07-23 02:38:21 -0500

    build: specify source and target compatibility java versions

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     `java-library`
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8;
+    targetCompatibility = JavaVersion.VERSION_1_8;
+}
+
 dependencies {
     api("org.jetbrains:annotations:21.0.1")
     api("team.unnamed.common:commons-validation:2.0.0-SNAPSHOT")
