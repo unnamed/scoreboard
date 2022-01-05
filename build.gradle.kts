73f1f66daff8194590b1d5cffc5e3b7032e39c39
sintaxis <joshua1504.jr@gmail.com>
2022-01-05 13:37:19 -0400

    build(gradle): add maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
-allprojects {
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
     repositories {
         mavenLocal()
         maven("https://repo.unnamed.team/repository/unnamed-public/")
@@ -6,5 +14,17 @@ allprojects {
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         maven("https://oss.sonatype.org/content/repositories/central")
         mavenCentral()
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group as String
+                artifactId = project.name
+                version = project.version as String
+
+                from(components["java"])
+            }
+        }
     }
 }
