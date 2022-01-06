a2bf2dc36004311fc2da5a1883cef29b8f8d8840
sintaxis <joshua1504.jr@gmail.com>
2022-01-05 20:07:16 -0400

    build: add 1.16 platform module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ include("api")
 project(":api").name = "scoreboard-api"
 
 // platforms
-sequenceOf("v1_8_R3").forEach {
+sequenceOf("v1_8_R3", "v1_16_R3").forEach {
     include("platform-$it")
     project(":platform-$it").name = "scoreboard-platform-$it"
 }
